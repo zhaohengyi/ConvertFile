b74be5c074b40b5345302089cdcf8888e47ad470
zhaohengyi <zhaohengyi@bvcomvc.com>
2012-11-05 20:02:13 +0800

    完成UI

---
+++ ConvertFile/ConvertFileDlg.cpp
@@ -0,0 +1,199 @@
+
+// ConvertFileDlg.cpp : 实现文件
+//
+
+#include "stdafx.h"
+#include "ConvertFile.h"
+#include "ConvertFileDlg.h"
+#include "afxdialogex.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// 用于应用程序“关于”菜单项的 CAboutDlg 对话框
+
+class CAboutDlg : public CDialogEx
+{
+public:
+	CAboutDlg();
+
+// 对话框数据
+	enum { IDD = IDD_ABOUTBOX };
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 支持
+
+// 实现
+protected:
+	DECLARE_MESSAGE_MAP()
+};
+
+CAboutDlg::CAboutDlg() : CDialogEx(CAboutDlg::IDD)
+{
+}
+
+void CAboutDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+BEGIN_MESSAGE_MAP(CAboutDlg, CDialogEx)
+END_MESSAGE_MAP()
+
+
+// CConvertFileDlg 对话框
+
+
+
+CConvertFileDlg::CConvertFileDlg(CWnd* pParent /*=NULL*/)
+	: CDialogEx(CConvertFileDlg::IDD, pParent)
+{
+	m_hIcon = AfxGetApp()->LoadIcon(IDR_MAINFRAME);
+}
+
+void CConvertFileDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+BEGIN_MESSAGE_MAP(CConvertFileDlg, CDialogEx)
+	ON_WM_SYSCOMMAND()
+	ON_WM_PAINT()
+	ON_WM_QUERYDRAGICON()
+	ON_BN_CLICKED(IDOPEN, &CConvertFileDlg::OnBnClickedOpen)
+	ON_BN_CLICKED(IDCONVERT, &CConvertFileDlg::OnBnClickedConvert)
+END_MESSAGE_MAP()
+
+
+// CConvertFileDlg 消息处理程序
+
+BOOL CConvertFileDlg::OnInitDialog()
+{
+	CDialogEx::OnInitDialog();
+
+	// 将“关于...”菜单项添加到系统菜单中。
+
+	// IDM_ABOUTBOX 必须在系统命令范围内。
+	ASSERT((IDM_ABOUTBOX & 0xFFF0) == IDM_ABOUTBOX);
+	ASSERT(IDM_ABOUTBOX < 0xF000);
+
+	CMenu* pSysMenu = GetSystemMenu(FALSE);
+	if (pSysMenu != NULL)
+	{
+		BOOL bNameValid;
+		CString strAboutMenu;
+		bNameValid = strAboutMenu.LoadString(IDS_ABOUTBOX);
+		ASSERT(bNameValid);
+		if (!strAboutMenu.IsEmpty())
+		{
+			pSysMenu->AppendMenu(MF_SEPARATOR);
+			pSysMenu->AppendMenu(MF_STRING, IDM_ABOUTBOX, strAboutMenu);
+		}
+	}
+
+	// 设置此对话框的图标。当应用程序主窗口不是对话框时，框架将自动
+	//  执行此操作
+	SetIcon(m_hIcon, TRUE);			// 设置大图标
+	SetIcon(m_hIcon, FALSE);		// 设置小图标
+
+	// TODO: 在此添加额外的初始化代码
+
+	return TRUE;  // 除非将焦点设置到控件，否则返回 TRUE
+}
+
+void CConvertFileDlg::OnSysCommand(UINT nID, LPARAM lParam)
+{
+	if ((nID & 0xFFF0) == IDM_ABOUTBOX)
+	{
+		CAboutDlg dlgAbout;
+		dlgAbout.DoModal();
+	}
+	else
+	{
+		CDialogEx::OnSysCommand(nID, lParam);
+	}
+}
+
+// 如果向对话框添加最小化按钮，则需要下面的代码
+//  来绘制该图标。对于使用文档/视图模型的 MFC 应用程序，
+//  这将由框架自动完成。
+
+void CConvertFileDlg::OnPaint()
+{
+	if (IsIconic())
+	{
+		CPaintDC dc(this); // 用于绘制的设备上下文
+
+		SendMessage(WM_ICONERASEBKGND, reinterpret_cast<WPARAM>(dc.GetSafeHdc()), 0);
+
+		// 使图标在工作区矩形中居中
+		int cxIcon = GetSystemMetrics(SM_CXICON);
+		int cyIcon = GetSystemMetrics(SM_CYICON);
+		CRect rect;
+		GetClientRect(&rect);
+		int x = (rect.Width() - cxIcon + 1) / 2;
+		int y = (rect.Height() - cyIcon + 1) / 2;
+
+		// 绘制图标
+		dc.DrawIcon(x, y, m_hIcon);
+	}
+	else
+	{
+		CDialogEx::OnPaint();
+	}
+}
+
+//当用户拖动最小化窗口时系统调用此函数取得光标
+//显示。
+HCURSOR CConvertFileDlg::OnQueryDragIcon()
+{
+	return static_cast<HCURSOR>(m_hIcon);
+}
+
+
+void CConvertFileDlg::OnBnClickedOpen()
+{
+	// TODO: 在此添加控件通知处理程序代码
+	CFileDialog *dlg = new CFileDialog(TRUE, _T("new"), _T("*.bin"),
+      OFN_FILEMUSTEXIST | OFN_HIDEREADONLY, _T("Raw Files (*.bin)|*.bin|All Files (*.*)|*.*||"));
+	if (IDOK == dlg->DoModal())
+	{
+		src = dlg->GetPathName();
+		SetDlgItemText(IDC_SRC, src);
+
+		delete(dlg);
+		((CButton*)GetDlgItem(IDCONVERT))->EnableWindow(TRUE);
+	}
+}
+
+void CConvertFileDlg::OnBnClickedConvert()
+{
+	// TODO: 在此添加控件通知处理程序代码
+	CString PathName;
+	CFileDialog *dlg = new CFileDialog(FALSE, _T("new"), _T("*.bin"),
+										OFN_FILEMUSTEXIST | OFN_HIDEREADONLY, 
+										_T("Raw Files (*.bin)|*.raw|All Files (*.*)|*.*||"));
+	if (IDOK == dlg->DoModal())
+	{
+		PathName = dlg->GetPathName();
+		SetDlgItemText(IDC_SAVE, PathName);
+		delete(dlg);
+	}
+
+	if (this->Convert(PathName))
+	{
+		MessageBox(_T("转换完成"));
+	}
+	else
+	{	
+		MessageBox(_T(" 转换错误"));
+	}
+}
+
+bool CConvertFileDlg::Convert(CString & dest)
+{
+	//转换文件，并写到新的文件在中
+	return false;
+}

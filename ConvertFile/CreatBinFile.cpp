952d7211384a6b23033696cb4db5ed1e0795bcb0
zhaohengyi <zhaohengyi@bvcomvc.com>
2012-11-14 19:35:53 +0800

    初步完成Bin文件代码编写，待测试

--- ConvertFile/CreatBinFile.cpp
+++ ConvertFile/CreatBinFile.cpp
@@ -1,38 +1,197 @@
 #include "StdAfx.h"
 #include "CreatBinFile.h"
 
-
 CCreatBinFile::CCreatBinFile(void)
 {
+	ImageBuf = NULL;
 	this->file = NULL;
 }
 
 CCreatBinFile::~CCreatBinFile(void)
 {
+	if (ImageBuf != NULL)
+	{
+		free(ImageBuf);
+		ImageBuf = NULL;
+	}
 }
 
 CCreatBinFile::CCreatBinFile(struct strFile *src)
 {
+	ImageBuf = (BYTE *)malloc(IMAGE_SIZE);
+	memset(ImageBuf, 0xff, IMAGE_SIZE);
 	this->file = src;
 }
 
-BOOL CCreatBinFile::GenerateBinFile()
-{
-	//BYTE *p = src;
-	//DWORD dwByteWrite = 0;
+#pragma region Read File
 
-	////创建输出文件
-	//HANDLE hNewFile = CreateFile(dest, GENERIC_WRITE, FILE_SHARE_READ, 0, \
-	//							CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, 0);
-	//if (hNewFile == INVALID_HANDLE_VALUE)
-	//	return false;
-	//
-	//WriteFile(hNewFile, p, 1, &dwByteWrite, NULL);
-	return true;
+BYTE *CCreatBinFile::ReadBinFile(CString file, DWORD *ReadSize)
+{
+	BYTE *ReadBuffer = NULL;
+	DWORD FileSize = 0;
+	DWORD hsize = 0;
+	HANDLE hFile = INVALID_HANDLE_VALUE;
+
+	//打开需要转换的文件
+	hFile =  CreateFile(this->file->OCCFilePath, GENERIC_READ, FILE_SHARE_READ, 0,\
+		OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, 0);
+
+	if (INVALID_HANDLE_VALUE == hFile)
+		goto _ERROR;
+
+	FileSize = GetFileSize(hFile, &hsize);
+
+	ReadBuffer = (BYTE *)malloc(FileSize + 10);
+	memset(ReadBuffer, '\0', FileSize + 10);
+
+	if( FALSE == ReadFile(hFile, (LPVOID)ReadBuffer, FileSize, ReadSize, NULL))
+	{
+		MessageBox(_T("读取文件错误 %wd"));
+		goto _ERROR;
+	}
+
+_ERROR:
+	return ReadBuffer;
 }
+
+BOOL CCreatBinFile::ReadMcsFile(void)
+{
+	if (this->file->MCSFilePath.IsEmpty())
+		return FALSE;
+
+	DWORD FileSize = 0;
+	BYTE *p = this->ReadBinFile(this->file->MCSFilePath, &FileSize);
+
+	memcpy_s((this->ImageBuf + this->McsAddr), this->McsSectionSzie, p, FileSize);
+
+	if (p != NULL)
+	{
+		free(p);
+		p = NULL;
+	}
+
+	return TRUE;
+}
+
+BOOL CCreatBinFile::ReadOCCFile(void)
+{
+	if (this->file->OCCFilePath.IsEmpty())
+		return FALSE;
+
+	DWORD FileSize = 0;
+	BYTE *pHead = this->ReadBinFile(this->file->MCSFilePath, &FileSize);
+	BYTE *p = pHead;
+	BYTE *pDest = (this->ImageBuf + this->OCCAddr);
+
+	for (DWORD i = 0; i < FileSize; i++)
+	{
+		if ((*p != 0x0D) && (*p != 0x0A))
+		{
+			pDest[i] = *p;
+		}
+	}
+
+	if (pHead != NULL)
+	{
+		p = NULL;
+		free(pHead);
+		pHead = NULL;
+	}	
+
+	return TRUE;
+}
+
+BOOL CCreatBinFile::ReadTwoPointFile(void)
+{
+	if (this->file->TwoPointFilePath)
+		return FALSE;
+
+	return FALSE;
+}
+
+BOOL CCreatBinFile::ReadVerticalFile(void)
+{
+	if (this->file->VerticalFilePath)
+		return FALSE;
+
+	return FALSE;
+}
+
+BOOL CCreatBinFile::ReadBlindFile(void)
+{
+	if (this->file->BlindFilePath)
+		return FALSE;
+
+	return FALSE;
+}
+
+BOOL CCreatBinFile::ReadMemsFile(void)
+{
+	if (this->file->OCCFilePath.IsEmpty())
+		return FALSE;
+
+	DWORD FileSize = 0;
+	BYTE *pHead = this->ReadBinFile(this->file->MemsFilePath, &FileSize);
+	BYTE *p = pHead;
+	BYTE *pDest = (this->ImageBuf + this->MemsAddr);
+	
+	BYTE seps[] = {0x0D, 0x0A};
+	char *next_token = NULL;
+	BYTE *token = (BYTE *)strtok_s((char *)pHead, (char *)seps, &next_token);
+
+	WORD offset = *((WORD *)token);
+
+	BYTE i = 0;
+	while (token != NULL)
+	{	
+		token = (BYTE *)strtok_s(NULL, (char *)seps, &next_token);
+		if (i == 0)
+		{
+			pDest += offset;
+			*((WORD *)pDest) = *((WORD *)token);
+			i++;
+		}
+		else
+		{
+			i = 0;
+			offset = *((WORD *)token);
+		}
+	}
+
+	if (pHead != NULL)
+	{
+		p = NULL;
+		free(pHead);
+		pHead = NULL;
+	}	
+
+	return TRUE;
+}
+#pragma endregion Read File
 
 
 BOOL CCreatBinFile::Generate()
-{
-	return false;
+{	
+	DWORD	dwByteWrite = 0;
+
+	this->ReadMcsFile();
+	this->ReadOCCFile();
+	this->ReadTwoPointFile();
+	this->ReadVerticalFile();
+	this->ReadBlindFile();
+	this->ReadMemsFile();
+
+	//创建输出文件
+	HANDLE hNewFile = CreateFile(this->file->DestFilePath, GENERIC_WRITE, FILE_SHARE_READ, 0, \
+								 CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, 0);
+	if ((hNewFile == INVALID_HANDLE_VALUE) || (hNewFile == NULL))
+		return false;
+	
+
+	WriteFile(hNewFile, this->ImageBuf, IMAGE_SIZE, &dwByteWrite, NULL);
+
+	if ((hNewFile != INVALID_HANDLE_VALUE) && (hNewFile != NULL))
+		CloseHandle(hNewFile);
+
+	return TRUE;
 }
--- ConvertFile/CreatBinFile.h
+++ ConvertFile/CreatBinFile.h
@@ -10,8 +10,27 @@ public:
 public:
 	BOOL Generate(void);
 private:
-	BOOL GenerateBinFile(void);
-private:
 	struct strFile *file;
+	BYTE *ImageBuf;
+
+	static const DWORD IMAGE_SIZE = 16*1024*1024;
+
+	static const DWORD McsAddr = 0x0;
+	static const DWORD OCCAddr = 0x400000;
+	static const DWORD TwoPointAddr = 0x900000;
+	static const DWORD VerticalAddr = 0xE80000;
+	static const DWORD BlindAddr = 0xF00000;
+	static const DWORD MemsAddr = 0xF80000;
+
+	static const DWORD McsSectionSzie = 4*1024*1024;
+private:
+	BYTE *ReadBinFile(CString file, DWORD *ReadSize);
+private:
+	BOOL ReadMcsFile(void);
+	BOOL ReadOCCFile(void);
+	BOOL ReadTwoPointFile(void);
+	BOOL ReadVerticalFile(void);
+	BOOL ReadBlindFile(void);
+	BOOL ReadMemsFile(void);
 };
 
